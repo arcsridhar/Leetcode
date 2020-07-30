1588a6536979ebe162f33f56a67ee4bfe7cdd16c
Archana Sridhar <61038056+arcsridhar@users.noreply.github.com>
2020-07-29 23:25:47 -0500

    Recursive Solution

--- Search in Binary Search Tree/Solution.java
+++ Search in Binary Search Tree/Solution.java
@@ -17,6 +17,7 @@ class Solution
 {
     public TreeNode searchBST(TreeNode root, int val) 
     {
+        //When there is no tree structure, return null
         if(root == null) return null;
         if(root.val == val) return root;
         if(root.val < val)
