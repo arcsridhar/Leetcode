ab22ce18070fa8d1e73e1f17b5e17390b2fa8f39
Archana Sridhar <61038056+arcsridhar@users.noreply.github.com>
2020-08-04 22:43:16 -0500

    Create Solution.java

---
+++ 3SumClosest/Solution.java
@@ -0,0 +1,31 @@
+class Solution 
+{
+    public int threeSumClosest(int[] nums, int target) 
+    {
+        int result = nums[0] + nums[1] + nums[nums.length-1];
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length-2; i++)
+        {
+            int low = i+1;
+            int high = nums.length-1;
+            while(low < high)
+            {
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum > target)
+                {
+                    high -= 1;
+                }
+                else
+                {
+                    low += 1;
+                }
+                if(Math.abs(sum-target) < Math.abs(result-target))
+                {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+}
